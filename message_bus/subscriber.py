9c94be7c3ce10527684fd417b1e441b60f40f593
Joseph Astier <jastier@gmail.com>
2022-04-20 16:22:39 -0700

    Added a Message Bus subscriber class

--- message_bus/subscriber.py
+++ message_bus/subscriber.py
@@ -1,60 +1,68 @@
 import paho.mqtt.client as mqtt
-from heartbeat_message import HeartbeatMessage
 
 # https://www.eclipse.org/paho/index.php?page=clients/python/docs/index.php
 
-# 
 
-class Coordinator():
-  heartbeat = HeartbeatMessage
-  def do_something_useful(self, client):
+class Subscriber():
+
+    def __init__(self):
+        print("Subscriber.__init__")
+        self.subscriber_client = SubscriberClient(self)
+        self.subscriber_client.startup()
+
+    def do_something_useful(self, client):
+        print("do_something_useful")
+
+# Asynchronous MQTT subscriber
+class SubscriberClient:
+
+    # TODO pass these in
+    host = "localhost"
+    port = 1883
+    keepalive = 60
+    bind_address = ""
+
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
+    def on_connect(client, userdata, flags, rc):
 
-    print("do_something_useful")
-
-
-class Mqtt:
-
-  # TODO pass these in
-  host = "localhost"
-  port = 1883
-  keepalive = 60
-  bind_address = ""
-
-  def on_connect(client, userdata, flags, rc):
-
-    # Paho return code definitions
-    if(rc == 0):
-      print("Connection successful")
-      userdata.do_something_useful(client)
-    elif(rc == 1):
-      print("Connection refused - incorrect protocol version")
-    elif(rc == 2):
-      print("Connection refused - invalid client identifier")
-    elif(rc == 3):
-      print("Connection refused - server unavailable")
-    elif(rc == 4):
-      print("Connection refused - bad username or password")
-    elif(rc == 5):
-      print("Connection refused - not authorised")
-    else:
-      print("Connection refused - return code " + str(rc))
+        # Paho return code definitions
+        if(rc == 0):
+            print("Connection successful")
+#            self.subscriber.do_something_useful(client)
+        elif(rc == 1):
+            print("Connection refused - incorrect protocol version")
+        elif(rc == 2):
+            print("Connection refused - invalid client identifier")
+        elif(rc == 3):
+            print("Connection refused - server unavailable")
+        elif(rc == 4):
+            print("Connection refused - bad username or password")
+        elif(rc == 5):
+            print("Connection refused - not authorised")
+        else:
+            print("Connection refused - return code " + str(rc))
 
 
-  # The callback for when a PUBLISH message is received from the server.
-  def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+    # The callback for when a PUBLISH message is received from the server.
+    def on_message(client, userdata, msg):
+        print(msg.topic + " " + str(msg.payload))
 
-  client = mqtt.Client(userdata=Coordinator())
-  client.on_connect = on_connect
-  client.on_message = on_message
+    def __init__(self, subscriber):
+        print("SubscriberClient.__init__")
+        self.subscriber = subscriber
 
-  print("TDAC connecting to Message Bus... ")
-  client.connect(host, port, keepalive, bind_address)
-  client.loop_start()
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
 
-  # Blocking call that processes network traffic, dispatches callbacks and
-  # handles reconnecting.
-  # Other loop*() functions are available that give a threaded interface and a
-  # manual interface.
+    def startup(self):
+        print("Subscriber connecting to Message Bus... ")
+        self.client.connect(
+            self.host, self.port, self.keepalive, self.bind_address)
+        self.client.loop_start()
+
+    # Blocking call that processes network traffic, dispatches callbacks and
+    # handles reconnecting.
+    # Other loop*() functions are available that give a threaded interface and a
+    # manual interface.
