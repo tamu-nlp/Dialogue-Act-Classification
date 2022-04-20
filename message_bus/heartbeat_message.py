3f08d1cfcbc4ecb369fc271d17db164d85569310
Joseph Astier <jastier@gmail.com>
2022-04-20 16:24:12 -0700

    Added a HeartbeatMessage class

---
+++ message_bus/heartbeat_message.py
@@ -0,0 +1,10 @@
+from common import CommonHeader
+import json
+
+class HeartbeatMessage:
+  pub_topic = "dialogue_act_classfier/heartbeat"
+
+  def message():
+    return "tdac_heartbeat_message"
+
+  print("HeartbeatMessage constructed")
