37fa6d5b6ea5afdbca9fc787e0aa9fb41a6c065b
Joseph Astier <jastier@gmail.com>
2022-05-05 14:19:12 -0700

    Removed class

--- message_bus/trial_message.py
+++
@@ -1,9 +0,0 @@
-import common
-import json
-
-class TrialMessage:
-
-    def __init__(self, message):
-        print("TrialMessage.__init__")
-        print("message = " + message)
-        self.foo = json.loads(message)
