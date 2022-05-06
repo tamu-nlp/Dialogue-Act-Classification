d1cc8d6bf53cd4abd2addccbb9cce03d1f70e150
Joseph Astier <jastier@gmail.com>
2022-05-06 12:12:18 -0700

    Timestamps are produced in format: YYYY-MM-DDThh:mm:ss.ssssZ

--- message_bus/utils.py
+++ message_bus/utils.py
@@ -1,3 +1,5 @@
+import datetime
+import re
 
 class Utils:
 
@@ -7,3 +9,16 @@ class Utils:
             value = src[key]
             if(value != None):
                 dst.update({key:value})
+
+    # return a UTC timestamp in format:  YYYY-MM-DDThh:mm:ss.ssssZ
+    def timestamp(self):
+        tm = datetime.datetime.utcnow()
+        p = "[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}[.]?[0-9]{4}"
+        iso = tm.isoformat(timespec='microseconds')
+        timestamp = str(iso)
+        m = re.search(p, timestamp)
+        if(m == None):
+            return timestamp + 'Z' # best guess if no match
+        else: 
+            return m.group(0) + 'Z'
+
