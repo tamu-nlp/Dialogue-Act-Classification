a4eef74acd28a70115df057731d6af9b4e45ad2d
Joseph Astier <jastier@gmail.com>
2022-06-08 12:00:40 -0700

    Changed some hardcoded TdacMessage field values

--- message_bus/tdac_message.py
+++ message_bus/tdac_message.py
@@ -2,10 +2,10 @@ from version import Version
 from message import Message
 
 class TdacMessage(Message):
-    topic = "agent/dialogue_act_classifier/"
+    topic = "agent/dialog_act_classifier/"
     message_type = "agent"
-    sub_type = "tdac"
-    source = "uaz_tdac_agent"
+    sub_type = "dialog_act_label"
+    source = "dialog_act_classifier"
     data = {
         "label" : "not_set",
         "asr_msg_id" : "not_set"
