0740d929eed6f7c9880baf5fa30bf1b7d047e594
Joseph Astier <jastier@gmail.com>
2022-06-15 18:50:26 -0700

    uptime now sent with rollcall_response, fixed comments

--- message_bus/message_bus.py
+++ message_bus/message_bus.py
@@ -4,6 +4,7 @@ from publisher import Publisher
 from heartbeat_publisher import HeartbeatPublisher
 from version import Version
 from version_info_message import VersionInfoMessage
+import datetime
 
 # Authors:  Joseph Astier, Adarsh Pyarelal
 #
@@ -14,6 +15,17 @@ class MessageBus():
 
     # shown on startup splash
     name = 'TDAC'
+
+    # keep track of when we start so we can compute uptime
+    tStart = datetime.datetime.utcnow()
+
+    # return the number of seconds this applicatio has been running
+    def uptime(self):
+        tNow = datetime.datetime.utcnow()
+        dt = tNow - self.tStart
+        dt_s = round(dt.total_seconds())
+
+        return dt_s
 
     def __init__(self, dac_server, host, port):
         self.dac_server = dac_server
@@ -34,18 +46,28 @@ class MessageBus():
     def classify_utterance(self, participant_id, text):
         return self.dac_server.classify_utterance(participant_id, text)
 
-    def start_trial(self, trial_message_d):
+    # enter running trial state
+    def start_trial(self, trial_d):
+        # send version info message
         version_info_message = VersionInfoMessage()
-        d = version_info_message.get_d(trial_message_d)
-        d['data'] = version_info_message.get_data()
+        d = version_info_message.get_d(self, trial_d)
         self.publish(d)
 
+        # reset the DAC
         self.dac_server.reset_model()
-        self.heartbeat_publisher.set_message_d(trial_message_d)
 
-    def stop_trial(self, trial_message_d):
+        # base heartbeats on the trial message (has trial id)
+        self.heartbeat_publisher.set_trial_d(trial_d)
+
+    # enter stopped trial state
+    def stop_trial(self, trial_d):
+
+        # reset the DAC
         self.dac_server.reset_model()
-        self.heartbeat_publisher.set_message_d(trial_message_d)
 
-    def publish(self, published_message_d):
-        self.publisher.publish(published_message_d)
+        # base heartbeats on the trial message (no trial id)
+        self.heartbeat_publisher.set_trial_d(trial_d)
+
+    # write to the message_bs
+    def publish(self, d):
+        self.publisher.publish(d)
