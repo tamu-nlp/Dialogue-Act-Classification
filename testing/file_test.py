e98bb61d76976ac941a58afcb1cc1001536dbf62
Joseph Astier <jastier@gmail.com>
2022-07-05 15:09:33 -0700

    Input file validity testing implemented

--- testing/file_test.py
+++ testing/file_test.py
@@ -8,8 +8,22 @@ import jsondiff
 
 # Compare two json files and output any differences.
 
-def test_files(file1, file2):
-    print('Testing ' + file1 + ', ' + file2)
+def diff_files(file1, file2):
+    print('Diffing ' + file1 + ', ' + file2)
+    return 0
+
+def valid_file(filename):
+    # Check that the file exists
+    if (os.path.isfile(filename)):
+        # Check that it's a metadata file
+        if filename.endswith('.metadata'):
+            return True
+        else:
+            print('Input file must have .metadata extension')
+            return False
+    else:
+        print('File not found.')
+        return False
 
 # If run as a script, test metadata files from the command line
 if __name__ == '__main__':
@@ -22,24 +36,9 @@ if __name__ == '__main__':
     args = parser.parse_args(sys.argv[1:])
 
     if len(args.filenames) == 2:
-        test_files(args.filenames[0], args.filenames[1])
+        if(valid_file(args.filenames[0])
+        and valid_file(args.filenames[1])):
+            diff_files(args.filenames[0], args.filenames[1])
+
     else:
         print('two filenames are required')
-
-    # process valid input files, warn on others
-    if False:
-
-        print(f'{filename}:')
-
-        # Check that the file exists
-        if (os.path.isfile(filename)):
-            # Check that it's a metadata file
-            if filename.endswith('.metadata'):
-                test_metadata_file(filename)
-            else:
-                print('Input file must have .metadata extension')
-        else:
-            print('File not found.')
-
-        print('')
-
