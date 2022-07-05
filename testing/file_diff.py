195ca812963a8d10563fa6202d637f8c5c3c6d85
Joseph Astier <jastier@gmail.com>
2022-07-05 15:46:55 -0700

    Robustified the input file validation

--- testing/file_diff.py
+++ testing/file_diff.py
@@ -8,24 +8,32 @@ import jsondiff
 
 # Compare two json files and output any differences.
 
-def diff_files(file1, file2):
-    print('Diffing ' + file1 + ', ' + file2)
-    return 0
+def diff_files(filename1, filename2):
+    print('Diffing ' + filename1 + ', ' + filename2)
 
+
+
+
+
+# exit(1) if file does not exist or does not have .metadata extension
 def valid_file(filename):
     # Check that the file exists
-    if (os.path.isfile(filename)):
-        # Check that it's a metadata file
-        if filename.endswith('.metadata'):
-            return True
-        else:
-            print('Input file must have .metadata extension')
-            return False
-    else:
-        print('File not found.')
-        return False
+    if not os.path.exists(filename):
+        print(f'{filename} not found')
+        exit(1)
 
-# If run as a script, test metadata files from the command line
+    # make sure it is a regular file or a link to one
+    if not os.path.isfile(filename):
+        print(f'{filename} must be a .metadata file')
+        exit(1)
+
+    # Check for metadata file extension
+    if not filename.endswith('.metadata'):
+        print(f'{filename} must have .metadata extension')
+        exit(1)
+
+
+# if run as a script, test metadata files from the command line
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
@@ -36,9 +44,11 @@ if __name__ == '__main__':
     args = parser.parse_args(sys.argv[1:])
 
     if len(args.filenames) == 2:
-        if(valid_file(args.filenames[0])
-        and valid_file(args.filenames[1])):
-            diff_files(args.filenames[0], args.filenames[1])
+        valid_file(args.filenames[0])
+        valid_file(args.filenames[1])
+
+        diff_files(args.filenames[0], args.filenames[1])
 
     else:
         print('two filenames are required')
+        exit(1)
