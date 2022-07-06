ef24b6c64defa15709aa67c0577c8b72bbcb39a1
Joseph Astier <jastier@gmail.com>
2022-07-05 18:28:53 -0700

    Added a test script for diffing JSON files

--- testing/file_diff.py
+++ testing/file_diff.py
@@ -8,26 +8,79 @@ import jsondiff
 
 # Compare two json files and output any differences.
 
-def diff_files(filename1, filename2):
-    print('Diffing ' + filename1 + ', ' + filename2)
+
+# return all the lines from one file
+def lines_from_file(filename):
+    try:
+        with open(filename, 'r', encoding='UTF-8') as input_file:
+            return input_file.readlines()
+
+    except IOError:
+        print (f'could not read lines from {filename}')
+        return ()
+
+def diff_pair(i, line0, line1):
+
+    json0 = json.loads(line0)
+    json1 = json.loads(line1)
+
+    d = jsondiff.diff(json0, json1, syntax='symmetric' )
+
+    if d:
+        print(f'Line {i}:')
+        print(f'{line0}')
+        print(f'{line1}')
+        print(json.dumps(d))
+        return 1
+
+    else:
+        return 0
+    
 
 
+def diff_files(filename0, filename1):
+    lines0 = lines_from_file(args.filenames[0])
+    lines1 = lines_from_file(args.filenames[1])
+    len0 = len(lines0)
+    len1 = len(lines1)
 
+    print(f'diffing {filename0}, {filename1}')
 
+    # bail out of line lists are not equal length
+    if len0 != len1:
+        print('Files do not contain the same number of lines')
+        print(f' Lines in {filename0}: {len0}')
+        print(f' Lines in {filename1}: {len1}')
+        exit(1)
+
+    # line pairs that had differences
+    diffs = 0
+
+    for x in range(len0):
+        diffs += diff_pair(x, lines0[x], lines1[x])
+
+    print(f'Lines tested: {len0}')
+    print(f'Non-matching: {diffs}')
+
+    if diffs == 0:
+        exit(0)
+    else:
+        exit(1)
 
 # exit(1) if file does not exist or does not have .metadata extension
 def valid_file(filename):
-    # Check that the file exists
+
+    # File must exist
     if not os.path.exists(filename):
         print(f'{filename} not found')
         exit(1)
 
-    # make sure it is a regular file or a link to one
+    # File must be a regular file or a link to one
     if not os.path.isfile(filename):
         print(f'{filename} must be a .metadata file')
         exit(1)
 
-    # Check for metadata file extension
+    # File must have .metadata extension
     if not filename.endswith('.metadata'):
         print(f'{filename} must have .metadata extension')
         exit(1)
