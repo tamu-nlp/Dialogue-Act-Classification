2c2e4c6e2331ddc13f63703bf87866ae5b5a875e
Joseph Astier <jastier@gmail.com>
2022-07-12 17:27:23 -0700

    Setting up testing

--- scripts/test_scripts/check_run.py
+++ scripts/test_scripts/check_run.py
@@ -26,35 +26,37 @@ def check_file(filename):
 def get_study_filenames(study):
     filenames = []
     src=f'gs://studies.aptima.com/study-3_2022/*{study}*.metadata'
-    completed_process = subprocess.run(['gsutil', 'ls', src], capture_output=True)
-    if completed_process.returncode == 0:
-        output=completed_process.stdout.decode('utf-8')
+    proc = subprocess.run(['gsutil', 'ls', src], capture_output=True)
+    if proc.returncode == 0:
+        output=proc.stdout.decode('utf-8')
         lines = output.split('\n')
         for line in lines:
             if len(line) > 0:
                 filenames.append(line)
 
     else:
-        print(completed_process.stderr.decode('utf-8'))
+        print(proc.stderr.decode('utf-8'))
 
     return filenames
 
 
-#def get_study(report, study):
-#    filenames = get_study_filenames(study)
-#    report['studies'].append({'name': study,'files':filenames})
-
-def get_tests(report):
+# add a list of tests to each study
+def prepare_tests(report):
     for i in range(0, len(report['studies'])):
         study = report['studies'][i]
         study['tests'] = []
-        filenames = get_study_filenames(study['name'])
+        study_name = study['name']
+        print(f'{study_name}:')
+        filenames = get_study_filenames(study_name)
         for j in range(0, len(filenames)):
-            study['tests'].append({'file':filenames[j]})
+            print(filenames[j])
+            study['tests'].append({'filename':filenames[j]})
 
-def get_studies(report, studies):
+# Add a list of studies to the report
+def get_studies(report, args):
     report['studies'] = []
-    for study in studies:
+    for study in args.studies:
+        # add the study name to the element
         report['studies'].append({'name': study})
 
 def present_report(report):
@@ -72,8 +74,8 @@ if __name__ == '__main__':
 
     # will contain all testing information 
     report = {}
-    get_studies(report, args.studies)
-    get_tests(report)
+    get_studies(report, args)
+    prepare_tests(report)
 
     # say report
     present_report(report)
